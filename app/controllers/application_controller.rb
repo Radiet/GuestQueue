3378c4e920ad5367310e37ca8d331c154493a34e
Radiet Billahirabba <radiet@41studio.com>
2016-10-28 17:49:17 +0700

    Add time zone setting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  around_action :set_time_zone
+
+private
+
+  def set_time_zone(&block)
+    Time.use_zone('Jakarta', &block)
+  end
 end
